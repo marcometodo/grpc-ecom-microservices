0b03f7862d9c872defee4447d56e8dbff96bff56
Marco Metodo <marco@overdose.digital>
2022-05-09 17:10:05 +1000

    Fix grpc dial address

--- product-service/pkg/client.go
+++ product-service/pkg/client.go
@@ -13,7 +13,7 @@ type ServiceClient struct {
 
 func InitServiceClient() pb.ProductServiceClient {
     // using WithInsecure() because no SSL running
-    cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+    cc, err := grpc.Dial("product_service:50051", grpc.WithInsecure())
 
     if err != nil {
         fmt.Println("Could not connect:", err)
